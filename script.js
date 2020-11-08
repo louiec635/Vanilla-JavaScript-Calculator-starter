d8bea1a24a9fbc23653b919496271db0c87c367b
chris_l <louiec635@gmail.com>
2020-11-08 14:52:52 -0500

    This is my first commit

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
+const numberButtons = document.querySelectorAll('[data-number]');
+numberButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        alert(button.innerText);
+    })
+})
+console.log(numberButtons);
 
+const operationButtons = document.querySelectorAll('[data-operation]');
+operationButtons.forEach(button =>{
+    button.addEventListener('click', ()=>{
+        alert(button.innerText);
+    })
+})
+
+console.log(operationButtons);
